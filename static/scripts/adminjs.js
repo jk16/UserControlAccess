f09d035a4917e0e88c4c5bc74a4564295107daf2
jk16 <dude16kh@gmail.com>
2016-02-24 17:28:57 -0500

    after password submitted: post, with data: user/pass.

--- static/scripts/adminjs.js
+++ static/scripts/adminjs.js
@@ -11,6 +11,31 @@ $(document).ready(function() {
 			$adminUser.hide();
 			$adminPass.show();
 		}
+		else {
+			e.preventDefault();
+			$adminPass.hide();
+
+
+			userandpass = {
+				user: $("#adminUser input").val(),
+				pass: $("#adminPass input").val()
+			};
+
+			//post to send login data to server
+			$.ajax({
+				type: 'POST',
+				url: '/admin',
+				data: userandpass,
+				success: function(response) {
+					console.log("response: " + response)
+				},
+				error: function(response) {
+					console.log(response)
+					alert('error')
+				}
+			})
+		}
+
 	});
 
 
