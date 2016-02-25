f866b5f12adc7236398dcd71d63789b73e73d248
jk16 <dude16kh@gmail.com>
2016-02-25 07:50:19 -0500

    Added UserPage handler, counter for login attempts

--- usercontrolaccess.py
+++ usercontrolaccess.py
@@ -2,28 +2,57 @@ import tornado.ioloop
 import tornado.web
 import json
 
+
+def printStuff(*args):
+    print(*args)
+
 class homePage(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
+
+class UserPage(tornado.web.RequestHandler):
+    def get(self, id_):
+        #validate user existence in JSON
+        user = "user" + id_
+        with open('data/data.json') as json_data:
+            d = json.load(json_data)
+            json_data.close()
+            printStuff(d)
+            user_list = d['users']
+
+        userInJSON = user in user_list
+        printStuff(not userInJSON)
+        self.render(
+            'user.html',
+            memberVal = "user" + str(id_),
+            userInJSON = userInJSON,
+            )
 
 class AdminPage(tornado.web.RequestHandler):
     def get(self):
         self.render("admin.html")
 
     def post(self):
+        count_login_attempts = 0
+
         username = self.get_argument('user')
         password = self.get_argument('pass')
         credsPassed = username == 'Admin' and password == '0'
 
         if (credsPassed):
-            response = {"user": username, "password": password}
-            self.write(json.dumps(response))                
+            response = {"success": True, "user": username, "password": password}
+            self.write(json.dumps(response))     
+        else:
+            count_login_attempts +=1
+            response = {"success": False, "user": username, "password": password}
+            self.write(json.dumps(response))           
 
 def make_app():
     handlers = [
             (r"/", homePage),
             (r"/admin", AdminPage),
             (r"/adminCreds", AdminPage),
+            (r"/user([0-9]+)", UserPage),
         ]
 
     return tornado.web.Application(handlers,debug=True,template_path='./templates',
@@ -40,7 +69,6 @@ if __name__ == "__main__":
              If you use it as an executable it will get called 
     """
     main()
-
 
 
 
