b103c4297cbe9ca44467b4c641fdf743e4ec67f6
jk16 <dude16kh@gmail.com>
2016-02-25 13:20:22 -0500

    logic for adminForm, POST with user/pass

--- static/scripts/adminjs.js
+++ static/scripts/adminjs.js
@@ -1,42 +1,47 @@
 $(document).ready(function() {
-	$adminForm = $("#adminForm");
-	$adminUser = $("#adminUser");
-	$adminPass = $("#adminPass");
+    $adminForm = $("#adminForm");
+    $adminUser = $("#adminUser");
+    $adminPass = $("#adminPass");
 
 
-	$adminForm.on('submit', function(e) {
-		if($adminForm.is(".userStep")) {
-			e.preventDefault();
-			$adminForm.removeClass("userStep");
-			$adminUser.hide();
-			$adminPass.show();
-		}
-		else {
-			e.preventDefault();
-			$adminPass.hide();
+    $adminForm.on('submit', function(e) {
+        if($adminForm.is(".userStep")) {
+            e.preventDefault();
+            $adminForm.removeClass("userStep");
+            $adminUser.hide();
+            $adminPass.show();
+        }
+        else {
+            e.preventDefault();
+            $adminPass.hide();
 
 
-			userandpass = {
-				user: $("#adminUser input").val(),
-				pass: $("#adminPass input").val()
-			};
+            userandpass = {
+                user: $("#adminUser input").val(),
+                pass: $("#adminPass input").val()
+            };
 
-			//post to send login data to server
-			$.ajax({
-				type: 'POST',
-				url: '/admin',
-				data: userandpass,
-				success: function(response) {
-					console.log("response: " + response)
-				},
-				error: function(response) {
-					console.log(response)
-					alert('error')
-				}
-			})
-		}
+            //post to send login data to server
+            $.ajax({
+                type: 'POST',
+                url: '/adminCreds',
+                data: userandpass,
+                success: function(response) {
+                    if(response.success) {
+                        // loadAdminHTML();
+                    }
+                    else {
 
-	});
+                    }
+                },
+                error: function(response) {
+                    alert("error");
+                    console.log("error --> response: " + JSON.parse(response));
+                }
+            });
+        }
+
+    });
 
 
 });
