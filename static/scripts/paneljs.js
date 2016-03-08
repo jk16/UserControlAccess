7d1e06efa9893e19db260a6b44da96fd4f1bdb6c
jk16 <dude16kh@gmail.com>
2016-03-07 21:39:46 -0500

    logic for adding a permission

--- static/scripts/paneljs.js
+++ static/scripts/paneljs.js
@@ -1,20 +1,38 @@
 $(document).ready(function(){
-    $permissionsForm = $("#permissionsForm");
-    $operationField = $("#operationField");
-    $specificField = $("#specificField");
+    $verifyPermissionsForm = $("#verifyPermissionsForm");
+    $operationToVerify = $("#operationToVerify");
+    $specificToVerify = $("#specificToVerify");
+    $addPermissionForm = $("#addPermissionForm");
+    $operationToAdd = $("#operationToAdd");
+    $specificToAdd = $("#specificToAdd");
 
-
-    $permissionsForm.on("submit", function(e){
+    $verifyPermissionsForm.on("submit", function(e){
         e.preventDefault();
-        var specific = $specificField.val();
-        var operation = $operationField.val();
+        var specific = $specificToVerify.val();
+        var operation = $operationToVerify.val();
         var message = {
             specific: specific,
             operation: operation
         };
         $.get("/hasPerm", message, function(response){
            var parsedResponse = JSON.parse(response);
-           alert(parsedResponse.has_permission)
+           console.log(parsedResponse.has_permission);
+        });
+    });
+
+    $addPermissionForm.on("submit", function(e) {
+        console.log('addPermissionForm called')
+        e.preventDefault();
+        var specific = $specificToAdd.val();
+        var operation = $operationToAdd.val();
+        var message = {
+            specific: specific,
+            operation: operation,
+        };
+
+        $.get("/addPerm", message, function(response){
+            var parsedResponse = JSON.parse(response);
+            console.log(parsedResponse);
         });
     });
 });
